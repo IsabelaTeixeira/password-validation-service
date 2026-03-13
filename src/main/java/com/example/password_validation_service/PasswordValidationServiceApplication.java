ba7de1adf38012b8ec94a92a45a01f4918fdf3c1
Isabela Teixeira <isabelateixeira003@gmail.com>
2026-03-13 16:47:42 -0300

    feat: initial spring boot project setup

---
+++ src/main/java/com/example/password_validation_service/PasswordValidationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.password_validation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordValidationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordValidationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/password_validation_service/PasswordValidationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.password_validation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordValidationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
