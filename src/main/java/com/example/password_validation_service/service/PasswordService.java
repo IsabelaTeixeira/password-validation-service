0de893be5784aeb84ab4b85323fba897aa28139b
Isabela Teixeira <isabelateixeira003@gmail.com>
2026-03-13 18:55:39 -0300

    feat: add PasswordService and PasswordValidator

---
+++ src/main/java/com/example/password_validation_service/service/PasswordService.java
@@ -0,0 +1,4 @@
+package com.example.password_validation_service.service;
+
+public class PasswordService {
+}
---
+++ src/main/java/com/example/password_validation_service/validator/PasswordValidator.java
@@ -0,0 +1,4 @@
+package com.example.password_validation_service.Validator;
+
+public class PasswordValidator {
+}
