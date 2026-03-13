2ac8aaaf21c137ed202ec6bb8ef57302654b7113
Isabela Teixeira <isabelateixeira003@gmail.com>
2026-03-13 17:50:28 -0300

    feat: add DTO PasswordRequest

---
+++ src/main/java/com/example/password_validation_service/dto/PasswordRequest.java
@@ -0,0 +1,12 @@
+package com.example.password_validation_service.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PasswordRequest {
+
+    private String password;
+
+}
